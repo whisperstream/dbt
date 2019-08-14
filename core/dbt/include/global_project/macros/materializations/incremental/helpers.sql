76cfa95be393876709297e10997e87312fc994cd
Drew Banin <drew@fishtownanalytics.com>
2019-08-13 22:15:25 -0400

    fix incremental return value

--- core/dbt/include/global_project/macros/materializations/incremental/helpers.sql
+++ core/dbt/include/global_project/macros/materializations/incremental/helpers.sql
@@ -1,22 +1,20 @@
 
 {% macro incremental_upsert(tmp_relation, target_relation, unique_key=none, statement_name="main") %}
-    {% set dest_columns = adapter.get_columns_in_relation(target_relation) %}
-    {% set dest_cols_csv = dest_columns | map(attribute='quoted') | join(', ') %}
+    {%- set dest_columns = adapter.get_columns_in_relation(target_relation) -%}
+    {%- set dest_cols_csv = dest_columns | map(attribute='quoted') | join(', ') -%}
 
-    {%- call statement(statement_name) -%}
-       {% if unique_key is not none -%}
-          delete
-          from {{ target_relation }}
-          where ({{ unique_key }}) in (
-            select ({{ unique_key }})
-            from {{ tmp_relation.include(schema=False, database=False) }}
-          );
-       {%- endif %}
+    {%- if unique_key is not none -%}
+    delete
+    from {{ target_relation }}
+    where ({{ unique_key }}) in (
+        select ({{ unique_key }})
+        from {{ tmp_relation.include(schema=False, database=False) }}
+    );
+    {%- endif %}
 
-       insert into {{ target_relation }} ({{ dest_cols_csv }})
-       (
-         select {{ dest_cols_csv }}
-         from {{ tmp_relation }}
-       );
-     {% endcall %}
+    insert into {{ target_relation }} ({{ dest_cols_csv }})
+    (
+       select {{ dest_cols_csv }}
+       from {{ tmp_relation }}
+    );
 {%- endmacro %}
